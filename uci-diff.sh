853a9319dc4f0816ffc4864cbf7e01cb8a3fc761
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2016-10-04 16:56:18 +0200

    added some more stuff

--- uci-diff.sh
+++ uci-diff.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 #Author: Reidar Cederqvist
+####### includes ######################
+#. /usr/share/libubox/jshn.sh
+####### end of includes ###############
 ####### helper functions ##############
 print_usage() {
 	echo -e "usage: $0 <action>\n"
 	echo -e "actions:\n"
-	echo -e "\t-d|--diff file1 file2 out-file         //create a JSON diff between file1 and file2 and print the result in out-file"
+	echo -e "\t-d|--diff diff-file orig-file out-file //create a JSON diff between file1 and file2 and print the result in out-file"
 	echo -e "\t-a|--apply file                        //apply a diff file"
 	echo -e "\t-e|--extract <config/all> out-file     //create a JSON file for the specified (or all) config(s)"
 	echo -e "\t-h|--help                              //print usage\n"
-}
-
-create_diff(){
-	echo $1
 }
 
 apply_diff(){
@@ -23,9 +22,17 @@ extract_config(){
 	if [ "$1" == "all" ]; then
 		echo -e '{' > $2
 		for file in $(ls /etc/config); do
-			echo -e "$(ubus call uci get "{\"config\":\"$file\"}" | sed "s/\"values\":/\"$file\":/g" | sed 's/^\t}$/\t},/g' | sed '/^[{}]$/d')" >>$2
+			echo -en "\rparsing $file                                 "
+			res="$(ubus call uci get "{\"config\":\"$file\"}" 2>/dev/null)"
+			if [ "$res" != "" ]; then
+				echo -e "$(echo -e "$res" | sed "s/\"values\":/\"$file\":/g" | sed 's/^\t}$/\t},/g' | sed '/^[{}]$/d')" >>$2
+			else
+				echo "\n\nERROR: Couldn't parse config: $file\n\n"
+			fi
 		done
-		echo -e '}' >>$2
+		sed -i '$ d' $2
+		echo -e '\t}\n}' >>$2
+		echo -e "\nFinished\n"
 	else
 		echo -e "$(ubus call uci get "{\"config\":\"$1\"}" | sed "s/\"values\":/\"$1\":/g")" >$2
 	fi
@@ -44,6 +51,7 @@ can_read(){
 case $1 in
 -d|--diff)
 	[ $# -ne 4 ] && print_usage && exit 1
+	[ "$2" == "$3" ] && print_usage && exit 1
 	can_read $2 || (echo -e "Can't read file: $2" && exit 1)
 	can_read $3 || (echo -e "Can't read file: $3" && exit 1)
 	can_write $4 || (echo -e "Can't write to file: $4" && exit 1)
